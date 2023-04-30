17f4d7eae5f998cb6b450f4eefa2971d2fc0e593
BerroElvi <131824842+BerroElvi@users.noreply.github.com>
2023-04-30 20:41:24 -0300

    Delete Curso Pa´lante/Clase 2 directory

--- Curso Pa´lante/Clase 2/main.js
+++
@@ -1,9 +0,0 @@
-const menu = document.querySelector('.menu');
-const lista = document.querySelector('.listamenu');
-
-
-
-menu.addEventListener('click', () => {
-
-    lista.classList.toggle('activado');
-});
